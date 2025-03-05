a72572f8f6f8a650b43992d0c27f0e448f9207d5
ectarreekon <ectarreekon@gmail.com>
2025-03-06 07:21:43 +0800

    Create service-worker.js

---
+++ static/service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'location-viewer-v1';
+const urlsToCache = [
+    '/',
+    '/static/styles.css',
+    '/manifest.json',
+    'https://unpkg.com/leaflet@1.7.1/dist/leaflet.css',
+    'https://unpkg.com/leaflet@1.7.1/dist/leaflet.js'
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
